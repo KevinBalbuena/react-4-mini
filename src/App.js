9e6cfeeb20c8f166d6cccf8a508ace5e2c57dc98
Kevin Balbuena <kbalbuena5@yahoo.com>
2019-08-27 14:18:58 -0700

    step 4 complete

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { HashRouter } from "react-router-dom";
 import Nav from "./Components/Nav/Nav";
+import routes from "./routes";
 import "./App.css";
 
 class App extends Component {
@@ -9,9 +10,7 @@ class App extends Component {
       <HashRouter>
         <div className="App">
           <Nav />
-          <h1 style={{ padding: "200px 35%" }}>
-            This is where your pages will appear
-          </h1>
+          {routes}
         </div>
       </HashRouter>
     );
